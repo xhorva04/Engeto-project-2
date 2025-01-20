a9c5e1fc50177394ee91ff8eec48e97ddf1868ef
xhorva04 <Tomas@horvath.site>
2025-01-20 16:57:40 +0100

    Kontrola opakovani cislic + revize skriptu. Final

--- main.py
+++ main.py
@@ -1,83 +1,69 @@
-# Druhy projekt do Engeto Online Python Akademie
-# author: Tomas Horvath
-# email: Tomas@horvath.site 
+# Druhý projekt do Engeto Online Python Akademie
+# Autor: Tomas Horvath
+# Email: Tomas@horvath.site 
 
-
-# Import balíčku pro náhodné volby
 import random
 
 
 def spustit_hru():
+    """Hlavní smyčka hry."""
     pokusy = 0
     pozdravit_uzivatele()
     tajne_cislo = generovani_tajneho_cisla()
+    
+
     while True:
         zadane_cislo = zadani_cisla()
         pokusy += 1
         if vyhodnot_hru(zadane_cislo, tajne_cislo):
+            print(f"🎉 Vyhrál jsi! 🎉 Počet pokusů: {pokusy}")
             break
-    print(f"Vyhral jsi!!")
-    print(f"Pocet pokusu {pokusy}")
-    # print(tajne_cislo)
 
 
-def pozdravit_uzivatele() -> str:
-    print("Vítejte ve hře Bulls & Cows")
+def pozdravit_uzivatele():
+    """Uvítání uživatele ve hře."""
+    print("Vítejte ve hře Bulls & Cows!")
     print("-" * 32)
-    print("Došlo k vygenerování 4ciferného čísla.")
+    print("Došlo k vygenerování 4ciferného čísla bez opakování číslic.")
+    print("Tvým úkolem je toto číslo uhodnout.")
     print("-" * 32)
 
 
-def prubeh_hry(hadani: bool):
-    un_seznam = random.sample(range(1, 9), 4)
-    nov = str()
-    print(un_seznam)
-    for i in un_seznam:
-        nov += str(i)
-    return int(nov)
+def generovani_tajneho_cisla() -> int:
+    """Generuje čtyřciferné číslo bez opakování číslic."""
+    cisla = list(range(10))
+    random.shuffle(cisla)
+    if cisla[0] == 0:  # Zajistíme, že číslo nezačíná nulou
+        cisla[0], cisla[1] = cisla[1], cisla[0]
+    tajne_cislo = int(''.join(map(str, cisla[:4])))
+    return tajne_cislo
 
 
-# Zadani cisla uzivatelem a jeho vyhodnoceni
 def zadani_cisla() -> int:
-    zadane_cislo = input("Zadej svuj tip: ")
-    if zadane_cislo.isdigit() and len(zadane_cislo) == \
-            4 and int(zadane_cislo[0]) > 0:
-        return int(zadane_cislo)
-    else:
-        print("Zadane cislo nesplnuje podminky, ukončuji program ...")
-       # quit()
+    """Zpracuje vstup od uživatele a vrátí validní čtyřciferné číslo."""
+    while True:
+        zadane_cislo = input("Zadej svůj tip (čtyřciferné číslo, nebo 'q' pro ukončení): ")
+        if zadane_cislo.lower() == 'q':  # Možnost ukončení hry
+            print("Hra ukončena. Děkujeme za hraní!")
+            exit()
+
+        if zadane_cislo.isdigit() and len(zadane_cislo) == 4 and len(set(zadane_cislo)) == 4:
+            if zadane_cislo[0] != '0':  # Číslo nesmí začínat nulou
+                return int(zadane_cislo)
+
+        print("Neplatný vstup. Zadej znovu čtyřciferné číslo bez opakování číslic.")
 
 
-# Vyhodnoceni hry zadane vs tajne cislo + formatovany vypis souteze
 def vyhodnot_hru(zadane: int, tajne: int) -> bool:
-    bull = 0
-    cow = 0
-    for i in range(4):
-        if str(tajne)[i] == str(zadane)[i]:
-            cow += 1
-        if str(zadane)[i] in str(tajne) and str(tajne)[i] != str(zadane)[i]:
-            bull += 1
+    """Porovná zadané číslo s tajným a vypíše výsledky."""
+    zadane = str(zadane)
+    tajne = str(tajne)
+    bulls = sum(1 for i in range(4) if zadane[i] == tajne[i])
+    cows = sum(1 for c in zadane if c in tajne) - bulls
 
-    print(f"{cow} cow{'s' if cow > 1 else ''} + {bull} bull{'s' if bull > 1 else ''}")
+    print(f"{bulls} bull{'s' if bulls != 1 else ''} + {cows} cow{'s' if cows != 1 else ''}")
     print("-" * 44)
-
-    return tajne == zadane
-
-
-def generovani_tajneho_cisla():
-    generated = []
-    nov = str()
-    cisla = list(range(1, 10))
-    generated.append(cisla.pop(random.randint(0, len(cisla) - 1)))
-
-    cisla.append(0)
-
-    for i in range(3):
-        generated.append(cisla.pop(random.randint(0, len(cisla) - 1)))
-
-    for cislo in generated:
-        nov += str(cislo)
-    return int(nov)
+    return bulls == 4
 
 
 if __name__ == '__main__':
